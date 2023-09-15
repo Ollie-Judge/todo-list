720937538252dfc8cd27fc7002d06cf81ef0b7b4
Ollie-Judge <ollie.judge@hotmail.com>
2023-09-15 14:03:59 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  //let todoItem = todoitem.body["todoItem"];
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
